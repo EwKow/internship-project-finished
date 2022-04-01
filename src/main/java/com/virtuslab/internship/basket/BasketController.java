015ddba6ba0ebdb965e589910765d8610583dba9
EwKow <ewelinab88@wp.pl>
2022-04-01 13:10:12 +0200

    added comment

--- src/main/java/com/virtuslab/internship/basket/BasketController.java
+++ src/main/java/com/virtuslab/internship/basket/BasketController.java
@@ -16,6 +16,12 @@ public class BasketController {
         this.basketService = basketService;
     }
 
+    /*sample entry:
+    POST http://localhost:8080/basket/add
+    Content-Type: application/json
+
+    ["Apple", "Orange", "Bread", "Bread", "Bread", "Cheese"]
+     */
     @PostMapping(value = "/add",consumes = "application/json")
     @ResponseBody
     public void addProductsToBasked(@RequestBody List<String> names){
